228a886f652585893fb1d5975cbdbaac54d51322
AlecTech <eremeevoleg@gmail.com>
2020-10-13 22:51:49 -0600

    question 5(challenge) done

--- practiceDay3.sql
+++ practiceDay3.sql
@@ -11,6 +11,9 @@ FROM employee INNER JOIN contact
 ON employee.ContactID = contact.ContactID
 WHERE contact.FirstName LIKE 'S%';
 
+-- Drop down list shows 31 
+
+
 -- 3.Q: Determine the current pay rate of the CEO of Adventure Works.
 SELECT employee.Title, employee.EmployeeID, employeepayhistory.Rate
 FROM employee INNER JOIN employeepayhistory
@@ -23,4 +26,48 @@ FROM employeedepartmenthistory INNER JOIN department
 ON employeedepartmenthistory.DepartmentID = department.DepartmentID
 WHERE employeedepartmenthistory.EndDate IS NULL
 GROUP BY department.DepartmentID, department.Name;
+
+-- 5.Challenge: Determine how many employees have a login ID ending with each number (0-9).
+
+SELECT
+
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%0') as Totals_0,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%1') as Totals_1,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%2') as Totals_2,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%3') as Totals_3,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%4') as Totals_4,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%5') as Totals_5,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%6') as Totals_6,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%7') as Totals_7,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%8') as Totals_8,
+(SELECT COUNT(employee.LoginID) 
+FROM employee
+WHERE  employee.LoginID LIKE '%9') as Totals_9;
+
+
+-- maybe substr makes is shorter
+SELECT SUBSTR(employee.LoginID,1,1) as Totals, count(employee.EmployeeID)
+FROM employee
+WHERE employee.LoginID LIKE '%0'
+GROUP BY SUBSTR(employee.LoginID, 1, 1);
+
+
 
